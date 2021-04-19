8be53c47260fae93116bb051729ea239943d0e33
Chánh Phan Xuân <2001190432@hufi.edu.vn>
2021-04-19 21:27:28 +0700

    Tiếp tục xây dựng class custom validate

--- Web/Validate/CustomValidate.cs
+++ Web/Validate/CustomValidate.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.UI.WebControls;
 
@@ -8,32 +9,44 @@ namespace Web.Validate
 {
     public class CustomValidate
     {
-        public static void InitCustomValidate(CustomValidator customValidator, ServerValidateEventArgs args)
-        {
-            customValidator.ErrorMessage = "";
-            customValidator.ServerValidate = new ServerValidateEventHandler();
-            customValidator.ClientValidationFunction = "";
+        //public static void InitCustomValidate(CustomValidator customValidator, ServerValidateEventArgs args)
+        //{
+        //    customValidator.ErrorMessage = "";
+        //    customValidator.ServerValidate = new ServerValidateEventHandler();
+        //    customValidator.ClientValidationFunction = "";
 
+        //}
+
+        public static void EmailValidate(object source, ServerValidateEventArgs args)
+        {
+            args.IsValid = false;
+            string value = args.Value;
+            if (Regex.IsMatch(value, "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"))
+                args.IsValid = true;
         }
 
-        public static bool EmailValidate(object source, ServerValidateEventArgs args)
+        public static void PasswordValidate(object source, ServerValidateEventArgs args)
         {
-            return false;
+            args.IsValid = false;
+            string value = args.Value;
+            if (Regex.IsMatch(value, @"((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})"))
+                args.IsValid = true;
         }
 
-        public static bool PasswordValidate(object source, ServerValidateEventArgs args)
+        public static void RePasswordValidate(object source, ServerValidateEventArgs args)
         {
-            return false;
+            args.IsValid = false;
+            string value = args.Value;
+            if (Regex.IsMatch(value, @"((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})"))
+                args.IsValid = true;
         }
 
-        public static bool RePasswordValidate(object source, ServerValidateEventArgs args)
+        public static void UsernameValidate(object source, ServerValidateEventArgs args)
         {
-            return false;
-        }
-
-        public static bool UsernameValidate(object source, ServerValidateEventArgs args)
-        {
-            return false;
+            args.IsValid = false;
+            string value = args.Value;
+            if (Regex.IsMatch(value, "^[a-z0-9_-]{3,15}$"))
+                args.IsValid = true;
         }
 
         public static bool PhoneNumberValidate(object source, ServerValidateEventArgs args)
