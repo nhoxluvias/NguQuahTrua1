46247b540b9e18ca5442a5c294f35563ff9c168f
Chánh Phan Xuân <2001190432@hufi.edu.vn>
2021-07-16 17:57:08 +0700

    Cập nhật lại connectionString

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -13,7 +13,7 @@ namespace Web
         protected void Application_Start(object sender, EventArgs e)
         {
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            DatabaseConfig.ManualConfig(@"MSI\SQL", "Movie", "sa", "123");
+            DatabaseConfig.ManualConfig(@"LAPTOP-B78E1G5S\MSSQLSERVER2019", "Movie", "sa", "123456789");
             MigrationConfig.Migrate();
             EmailConfig.RegisterEmail();
         }
